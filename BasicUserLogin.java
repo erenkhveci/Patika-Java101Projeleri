5d9dc624beaef8aa2587e17723d8a78be1954b27
Eren Kahveci <erenkhveci@hotmail.com>
2023-02-12 17:25:15 +0300

    Add files via upload

---
+++ BasicUserLogin.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class BasicUserLogin {
+    public static void main(String[] args) {
+        String userName,password,sifirlama,newUserName,newPassword;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Kullanıcı adınızı giriniz :");
+        userName= sc.nextLine();
+        System.out.print("Kullanıcı şifrenizi giriniz :");
+        password= sc.nextLine();
+        if(userName.equals("patika")&& password.equals("java123")){
+            System.out.println("Giriş Yaptınız.");
+        }else{
+            System.out.println("Bilgillerinizi yanlış girdiniz!");
+            System.out.print("Şifrenizi Sıfırlamak İster Misiniz? (E veya H)");
+            sifirlama=sc.nextLine();
+            if(sifirlama.equals("E")){
+                System.out.print("Yeni Şifrenizi Giriniz :");
+                newPassword=sc.nextLine();
+                if(!(newPassword.equals("java123"))){
+                    password=newPassword;
+                    System.out.println("Yeni Şifreniz Oluşturuldu");
+                }else {
+                    System.out.println("Şifre oluşturulamadı, lütfen başka şifre giriniz.");
+
+                }
+            }else if(sifirlama.equals("H")){
+                System.out.println("Have a Nice Day(Bon Jovi)");
+            }else{
+                System.out.println("E veya H şeklinde giriniz");
+            }
+        }
+    }
+}
