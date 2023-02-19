23b36c805ec10703d2d25a8a9d0afcffeeaceb70
Eren Kahveci <erenkhveci@hotmail.com>
2023-02-19 15:01:42 +0300

    Add files via upload

---
+++ ArrayHarmocicSum.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class ArrayHarmocicSum{
+    static double arrayHarmonicSum(int[] arr){
+        double harmonic=1;
+        for (int i=0;i<arr.length;i++){
+            harmonic +=1/arr[i];
+        }
+       double harmonicAvarage=arr.length/harmonic;
+        return harmonicAvarage;
+    }
+
+
+    public static void main(String[] args) {
+        int[] list={1,2,3,4,5,6,7,8,9};
+        System.out.println("Arraydeki sayıların harmonik ortalaması :"+(arrayHarmonicSum(list)));
+    }
+
+}
+
+
+
+
